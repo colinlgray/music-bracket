109bc28c272fdcc6b3b2f8832c347ea241934fab
Colin Gray <colin@cgray.dev>
2019-05-11 20:55:10 -0700

    added build to repo for deployment to heroku

---
+++ server/build/precache-manifest.2e9014adc4b1f01c7c6f7ed16fed11a0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ced2fb5b2e65d1b311655eb7e3565973",
+    "url": "/index.html"
+  },
+  {
+    "revision": "104e25ac0b28e64846ed",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "c99cfa219627007797d1",
+    "url": "/static/js/2.211cd7c6.chunk.js"
+  },
+  {
+    "revision": "104e25ac0b28e64846ed",
+    "url": "/static/js/main.ef5e865e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
