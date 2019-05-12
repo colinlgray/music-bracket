4a6cca928e6288d5bfecb381065de4b80a922043
Colin Gray <colin@cgray.dev>
2019-05-11 21:23:26 -0700

    redirect all

--- server/src/index.js
+++ server/src/index.js
@@ -3,13 +3,18 @@ const bodyParser = require("body-parser");
 const path = require("path");
 const app = express();
 app.use(express.static(path.join(__dirname, "..", "build")));
+app.use(redirectUnmatched)
+
+function redirectUnmatched(req, res) {
+  res.redirect("/");
+}
 
 app.get("/ping", function(req, res) {
   return res.send("pong");
 });
 
 app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, ".." / "build", "index.html"));
+  res.sendFile(path.join(__dirname, ".." , "build", "index.html"));
 });
 
 app.listen(process.env.PORT || 8080);
