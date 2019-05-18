ebfb01d5b7e6a9d13d6dc1896f69836aedd27d04
Colin Gray <colin@cgray.dev>
2019-05-17 22:57:50 -0700

    removed logs

--- server/src/index.js
+++ server/src/index.js
@@ -6,11 +6,9 @@ const app = express();
 const api = require("./api");
 
 app.use(bodyParser.json());
-// app.use(cors());
 app.use(express.static(path.join(__dirname, "..", "..", "build")));
 app.get("/api/songs", (req, res) => {
   api.getSongs({ query: req.query }).then(songs => {
-    // console.log("", songs);
     res.send(songs);
   });
 });
