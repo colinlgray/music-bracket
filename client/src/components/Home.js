239fe4060c6b2a205b40085c7f996562c00e57b0
Colin Gray <colin@cgray.dev>
2019-05-25 14:09:50 -0700

    another change to recompile

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -5,7 +5,7 @@ function Home() {
   return (
     <>
       <Typography component="h3" variant="h3" color="inherit" gutterBottom>
-        Welcome to a website
+        Welcome
       </Typography>
       <Typography component="div" style={{ paddingTop: "24px" }}>
         To the left is the menu where you can nominate your favorite song, then
