2cd7ae989431d938fbcdfd9ea33827514744f9e5
Colin Gray <colin@cgray.dev>
2019-06-18 19:42:54 -0700

    merged correctly

--- server/src/api.test.js
+++ server/src/api.test.js
@@ -1,35 +1,31 @@
 import { app } from "../src";
 import request from "supertest";
 
-describe("GET /api/artists", () => {
-  it("responds with json", () => {
-    return request(app)
-      .get("/api/artists")
-      .set("Accept", "application/json")
-      .expect("Content-Type", /json/)
-      .expect(200)
-      .then(response => {
-        expect(response.body.length).toEqual(1);
-        expect(response).toBeTruthy();
-      });
-  });
+test("GET /api/artists", () => {
+  return request(app)
+    .get("/api/artists")
+    .set("Accept", "application/json")
+    .expect("Content-Type", /json/)
+    .expect(200)
+    .then(response => {
+      expect(response.body.length).toEqual(1);
+      expect(response).toBeTruthy();
+    });
 });
 
-describe("GET /api/artists/:id", () => {
-  it("responds with json", () => {
-    const id = "7jVv8c5Fj3E9VhNjxT4snq";
-    return request(app)
-      .get(`/api/artists/${id}`)
-      .set("Accept", "application/json")
-      .expect("Content-Type", /json/)
-      .expect(200)
-      .then(response => {
-        expect(response.body.id).toEqual(id);
-      });
-  });
+test("GET /api/artists/:id", () => {
+  const id = "7jVv8c5Fj3E9VhNjxT4snq";
+  return request(app)
+    .get(`/api/artists/${id}`)
+    .set("Accept", "application/json")
+    .expect("Content-Type", /json/)
+    .expect(200)
+    .then(response => {
+      expect(response.body.id).toEqual(id);
+    });
 });
 
-describe("GET /api/tracks/search", () => {
+test("GET /api/tracks/search", () => {
   const query = "old town road";
   const limit = 10;
   const offset = 0;
@@ -37,14 +33,12 @@ describe("GET /api/tracks/search", () => {
     query
   )}&limit=${limit}&offset${offset}`;
 
-  it("responds with json", () => {
-    return request(app)
-      .get(url)
-      .set("Accept", "application/json")
-      .expect("Content-Type", /json/)
-      .expect(200)
-      .then(response => {
-        expect(response).toBeTruthy();
-      });
-  });
+  return request(app)
+    .get(url)
+    .set("Accept", "application/json")
+    .expect("Content-Type", /json/)
+    .expect(200)
+    .then(response => {
+      expect(response).toBeTruthy();
+    });
 });
