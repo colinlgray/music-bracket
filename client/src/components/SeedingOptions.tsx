bcf6cd75e9d5e2c2ed8ce25730df89b0c1c48f17
Colin Gray <colin@cgray.dev>
2019-07-29 16:51:42 -0700

    Another minor styling change

--- client/src/components/SeedingOptions.tsx
+++ client/src/components/SeedingOptions.tsx
@@ -1,5 +1,21 @@
 import React from "react";
-
+import Typography from "@material-ui/core/Typography";
+import { makeStyles, Theme } from "@material-ui/core/styles";
+const useStyles = makeStyles((theme: Theme) => ({
+  title: {
+    paddingLeft: theme.spacing(2)
+  }
+}));
 export function SeedingOptions(props: {}) {
-  return <div>Seed By</div>;
+  const classes = useStyles();
+  return (
+    <Typography
+      component="div"
+      color="inherit"
+      gutterBottom
+      className={classes.title}
+    >
+      Seed By:
+    </Typography>
+  );
 }
