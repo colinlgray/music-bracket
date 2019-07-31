a4a4545deca25c0c35c26f52b23c03298be43fbe
Colin Gray <colin@cgray.dev>
2019-07-30 17:34:14 -0700

    Added dropdown for seeding, not functional

--- client/src/components/SeedingOptions.tsx
+++ client/src/components/SeedingOptions.tsx
@@ -1,13 +1,28 @@
-import React from "react";
+import React, { ChangeEvent } from "react";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles, Theme } from "@material-ui/core/styles";
+import { Select } from "@material-ui/core";
+import MenuItem from "@material-ui/core/MenuItem";
+import InputLabel from "@material-ui/core/InputLabel";
+import FormControl from "@material-ui/core/FormControl";
+
 const useStyles = makeStyles((theme: Theme) => ({
   title: {
     paddingLeft: theme.spacing(2)
   }
 }));
+const options = [
+  { value: "custom", displayText: "Custom" },
+  { value: "popularity", displayText: "Popularity" }
+];
 export function SeedingOptions(props: {}) {
   const classes = useStyles();
+  const [value, setValue] = React.useState(options[0].value);
+
+  function handleChange(event: React.ChangeEvent<{ value: unknown }>) {
+    setValue(event.target.value as string);
+  }
+
   return (
     <Typography
       component="div"
@@ -15,7 +30,24 @@ export function SeedingOptions(props: {}) {
       gutterBottom
       className={classes.title}
     >
-      Seed By:
+      <FormControl>
+        <InputLabel htmlFor="seed-by">Seed By</InputLabel>
+        <Select
+          inputProps={{
+            id: "seed-by"
+          }}
+          value={value}
+          onChange={handleChange}
+        >
+          {options.map(i => {
+            return (
+              <MenuItem key={i.value} value={i.value}>
+                {i.displayText}
+              </MenuItem>
+            );
+          })}
+        </Select>
+      </FormControl>
     </Typography>
   );
 }
